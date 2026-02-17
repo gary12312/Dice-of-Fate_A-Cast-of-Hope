ff665a196a4ace940925d0789ef24231428e1b79
gary12312 <127240408+gary12312@users.noreply.github.com>
2026-02-17 21:01:42 +0300

    v.0.44H

---
+++ Assets/_DiceFate/Scripts/Loot/RockOnGround.cs
@@ -0,0 +1,107 @@
+using DiceFate.Units;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+namespace DiceFate.Loot
+{
+    public class RockOnGround : MonoBehaviour, ISelectable, IHover
+    {
+        [field: SerializeField] public bool IsSelected { get; private set; }   // ISelectable
+        [field: SerializeField] public bool IsHover { get; private set; }   // IHover
+
+        [SerializeField] private GameObject particle;
+
+        //[SerializeField] private LootTwinAnimation lootTwinAnimation;
+        [SerializeField] private ObjectOutline Outline; //обводка
+
+        private bool isActivate;
+
+
+        private void Start()
+        {
+            InitializationСheck();
+            InitializationStart();
+        }
+
+
+        private void InitializationStart()
+        {
+            isActivate = false;
+            particle.SetActive(false);
+            Outline?.DisableOutline();                // 2. отключить Обводку          
+        }
+
+
+        public void ActivateRock()
+        { 
+            isActivate = true;        
+            particle.SetActive(true);        
+        }
+
+
+
+        //-------------- IHover реализация --------------
+
+        public void OnEnterHover()
+        {
+            if (isActivate)
+            {
+                if (IsSelected == false) { Outline?.EnableOutline(); }
+                IsHover = true;
+            }
+        }
+        public void OnExitHover()
+        {
+            if (isActivate)
+            {
+                if (IsSelected == false) { Outline?.DisableOutline(); }
+            IsHover = false;
+                }
+        }
+        //-------------- Управление обводкой юнита --------------
+        public void OutlineOnSelected()
+        {
+            Outline?.EnableOutline();
+            // Outline?.ChangeColorOutline(UnitSO.colorSelected);
+        }
+        public void OutlineOffSelected()
+        {
+            // Outline?.ResetColorOutline();
+            Outline?.DisableOutline();
+        }
+
+
+
+
+        //-------------- ISelectable реализация --------------
+        public void Select()
+        {
+            //if (lootTwinAnimation != null)
+            //{
+            //    lootTwinAnimation.OpenLoot();
+            //}
+        }
+        public void Deselect()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void JumpBeforeAttack()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UnitsSOCurrent(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void InitializationСheck()
+        {
+            //if (lootTwinAnimation == null) { Debug.LogError($"Установить lootTwinAnimation для {this} "); }
+            if (Outline == null) { Debug.LogError($"Установить Outline для {this} "); }
+        }
+
+    }
+}
